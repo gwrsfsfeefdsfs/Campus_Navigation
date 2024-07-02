c5dffe44adc42098e4b72a9bbc9bbba9bf1e8f73
HDLK233 <1187411051@qq.com>
2024-07-02 13:12:09 +0800

    233

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include <graphics.h>		// 引用图形库头文件
 #include <conio.h>
+#include <graphics.h>// 引用图形库头文件
+#include <iostream>
 using namespace std;
-
 int main(){
-    initgraph(640, 480);	// 创建绘图窗口，大小为 640x480 像素
-    circle(200, 200, 100);	// 画圆，圆心(200, 200)，半径 100
+    initgraph(1000, 1000);	// 创建绘图窗口，大小为 640x480 像素
     _getch();				// 按任意键继续
     closegraph();			// 关闭绘图窗口
    //2233
