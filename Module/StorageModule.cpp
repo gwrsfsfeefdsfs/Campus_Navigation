3eacdc1a0aefdbe29524e963cd7549bdbe9eedd9
Apollyon <124168018+gwrsfsfeefdsfs@users.noreply.github.com>
2024-07-08 11:39:57 +0800

    修改建筑节点信息插入函数名字

--- Module/StorageModule.cpp
+++ Module/StorageModule.cpp
@@ -14,7 +14,7 @@ StorageModule::StorageModule() {
 void StorageModule::insertNode(const std::string& name, double x, double y, int type) {
     nodes[name] = {name, static_cast<short>(x), static_cast<short>(y), type};
 }
-void StorageModule::insertNode2( const std::string& name,const std::string& imgepath2, const std::string& imagetext2){
+void StorageModule::insertNode_building( const std::string& name,const std::string& imgepath2, const std::string& imagetext2){
     nodes[name].imagepath = imgepath2;
     nodes[name].imagetext = imagetext2;
 }
@@ -147,7 +147,9 @@ void StorageModule::generateTestData() {
     insertNode("乐学路拐点8",408,120,2);
     insertNode("乐学路青年教工楼",463,112,3);
     insertNode("淑苑",408,65,3);
+    //insertNode_building
     insertNode("北门",337,52,3);
+    //insertNode_building
 //-----
 
 //燕园路区域
@@ -161,6 +163,8 @@ void StorageModule::generateTestData() {
     insertNode("燕园路拐点6",498,435,2);
     insertNode("燕园路与正德路交点",445,461,1);
     insertNode("南1门",443,542,3);
+    //insertNode_building
+
 //-----
     //厚学路区域
     insertNode("厚学路与正德路交点",443,403,1);
@@ -220,12 +224,19 @@ void StorageModule::generateTestData() {
 
     //新增结点及道路
     insertNode("清苑",250,46,3);
+    //insertNode_building
     insertNode("汀苑",275,118,3);
+    //insertNode_building
     insertNode("桃李园",138,100,3);
+    //insertNode_building
     insertNode("花江运动区",378,103,3);
+    //insertNode_building
     insertNode("商业街",383,139,3);
+    //insertNode_building
     insertNode("中央食堂",360,152,3);
+    //insertNode_building
     insertNode("四创中心",269,210,3);
+    //insertNode_building
 
     insertNode("力港大道新增结点1",237,328,1);
     insertNode("紫荆路新增结点1",109,303,1);
